ca120da6249fde14cba95d44a0612d25a9aec5ac
tknicus <rayhargg69@gmail.com>
2026-08-19 12:44:04 +0800

    Update database.js

--- database.js
+++ database.js
@@ -1,7 +1,9 @@
 // KINI ANG IMONG OFFLINE DATABASE
 const riderDatabase = [
-  { idNum: "1001", fullname: "Juan Dela Cruz", nickname: "Juan", level: "G5", category: "Big Bike" },
-  { idNum: "1002", fullname: "Pedro Penduko", nickname: "Pedz", level: "G1", category: "Scooter" },
-  { idNum: "1234", fullname: "Prof G", nickname: "Master", level: "SKM26", category: "Enduro" }
+  { idNum: "1001", fullname: "Juan Dela Cruz", nickname: "Juan", level: "G5", category: "Adventure 1000cc", classification: "Veteran" },
+  { idNum: "1002", fullname: "Pedro Penduko", nickname: "Pedz", level: "SKM26", category: "Scooter 155cc", classification: "Novice" },
+  { idNum: "1234", fullname: "Prof G", nickname: "Master", level: "SKM26", category: "Enduro", classification: "Novice" },
+  { idNum: "2026", fullname: "Master Prof", nickname: "Prof G", level: "G0D", category: "Enduro 250cc", classification: "Master" },
+  // Dinhi nimo isumpay ang ubang riders...
   // I-copy paste ra nimo dinhi ang ubang riders
 ];
