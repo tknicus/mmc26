388bc7475b9000a3594e2d792e73a9a881069d8d
tknicus <rayhargg69@gmail.com>
2026-08-20 01:55:35 +0800

    Update service worker cache version and logic

--- sw.js
+++ sw.js
@@ -1,12 +1,10 @@
-const CACHE_NAME = 'mmc26-cache-v1';
+const CACHE_NAME = 'mmc26-cache-v2';
 
 // KINI ANG MGA FILES NGA I-DOWNLOAD UG I-SAVE SA SELPON INIG UNANG ABLI
 const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
-  './database.js', // <--- Idugang ni dinhi!
-  './logo.png', // <--- IDUGANG KINI DINHI!
   'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/sweetalert2@8',
   'https://lh3.googleusercontent.com/d/1m1NrFKOMKh4YjoUdXD0KvxcpySM5RuwU' // Ang imong banner image
@@ -26,7 +24,7 @@ self.addEventListener('install', event => {
 // FETCHING: Inig abli sa rider sa bukid nga walay internet, i-serve ang naka-save nga files
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
+    caches.match(event.request, { ignoreSearch: true })
       .then(response => {
         // Kung na-save na sa cache, mao nay i-gawas. Kung wala, i-download gikan sa internet.
         return response || fetch(event.request);
