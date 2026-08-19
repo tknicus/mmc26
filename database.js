4d9bcb346e403daf39e453186279632825d54fb8
tknicus <rayhargg69@gmail.com>
2026-08-19 12:27:16 +0800

    Update database.js

--- database.js
+++ database.js
@@ -1,1 +1,7 @@
-
+// KINI ANG IMONG OFFLINE DATABASE
+const riderDatabase = [
+  { idNum: "1001", fullname: "Juan Dela Cruz", nickname: "Juan", level: "G5", category: "Big Bike" },
+  { idNum: "1002", fullname: "Pedro Penduko", nickname: "Pedz", level: "G1", category: "Scooter" },
+  { idNum: "1234", fullname: "Prof G", nickname: "Master", level: "SKM26", category: "Enduro" }
+  // I-copy paste ra nimo dinhi ang ubang riders
+];
